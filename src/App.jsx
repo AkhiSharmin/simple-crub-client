2abae1478896307c9cbd884df7251d0bbb5a706d
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-11 03:01:45 +0600

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form>
+        <input type="text" name="name" id="name" />
+        <br />
+        <input type="email" name="email" id="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
