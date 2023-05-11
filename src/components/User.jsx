ed3672bc7abd4b1c8afd82cf4aebbd36d3cd2345
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-12 04:04:05 +0600

    added update button

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const User = () => {
   const users = useLoaderData();
 
+  const [myUsers, setUser] = useState(users);
+
   const handelDelete = (_id) => {
     console.log("delete", _id);
-    fetch(`http://localhost:5000/users/${_id}`);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("deleted successfully");
+          const remaining = myUsers.filter((myUser) => myUser._id !== _id);
+          setUser(remaining);
+        }
+      });
   };
 
   return (
@@ -15,7 +28,10 @@ const User = () => {
       <div>
         {users.map((user) => (
           <p key={user._id}>
-            {user.name} {user.email}{" "}
+            {user.name} {user.email}
+            <Link to={`/update/${user._id}`}>
+              <button>Update</button>
+            </Link>
             <button onClick={() => handelDelete(user._id)}>X</button>
           </p>
         ))}
